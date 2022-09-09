8039306bd6bcc45299921f598860f8b11daf26c7
mikky-999 <michaeldaudu11@gmail.com>
2022-09-08 22:36:25 -0700

    task five correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**                                                                                                                                     
- *
  *  main - Print out string using printf
  *
  *  Description: Using the main function
  *  This program prints with proper grammar,
- *   but the outcome is a piece of art,
+ *  but the outcome is a piece of art,
  *   return: 0
  */
 
