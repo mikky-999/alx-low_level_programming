cf7f13e6453600e66f698371df98965056147143
mikky-999 <michaeldaudu11@gmail.com>
2022-09-10 11:11:25 -0700

    3 to 6

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lu;
+	for (lu = 'a' ; lu <= 'z' ; lu++)
+		putchar(lu);
+	for (lu = 'A' ; lu <= 'Z'; lu++)
+		putchar(lu);
+	putchar('\n');
+	return (0);
+}
