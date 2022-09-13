fc24dbdc303a08fcd48b8835b2a4d02c50265040
mikky-999 <michaeldaudu11@gmail.com>
2022-09-13 14:33:52 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs(int) -Find the absolute number
+ * @n: integer argument to be passed
+ * Return: (n)
+ */
+int _abs(int n)
+{
+	if (n > '0' || n == '0')
+	{
+		return (n);
+	}
+	else
+	{
+		return (n / -1);
+	}
+}
