c2a3085717a89239458e090839ece35b307a1469
mikky-999 <michaeldaudu11@gmail.com>
2022-09-14 13:20:15 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main'h"
+/**
+ * print_to_98 - print all numbers from n to 98
+ *
+ *@n: interger argument paased into the function
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+			n--;
+	}
+	else
+	{
+		while (n >= 98)
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+			n++;
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,22 +7,21 @@
 void times_table(void)
 {
 	int num, mul, ans;
+
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+
 		for (mul = 1; mul <= 9; mul++)
 		{
 			_putchar(',');
 			_putchar(' ');
 			ans = num * mul;
 			if (ans <= 9)
-			{
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar((ans / 10) + '0');
-			}
+			_putchar((ans % 10) + '0');
 		}
 		_putchar('\n');
 	}
