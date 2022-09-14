1480428e9de558a3f1162bd13dc40c2c3ade21bd
mikky-999 <michaeldaudu11@gmail.com>
2022-09-13 19:28:38 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - multiplication table to 9
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int num, mul, ans;
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			ans = num * mul;
+			if (ans <=9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((ans / 10) + '0');
+
+			}
+		}
+		_putchar('\n');
+	}
+}
+
