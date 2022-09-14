ccfece0cc25abd51ebac937204136871a67590c0
mikky-999 <michaeldaudu11@gmail.com>
2022-09-13 18:14:19 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main"
+/**
+ * add - Add two integers
+ * @n1: the first argument passed
+ * @n2: the second argument passed
+ * Return: the result of the addition
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
