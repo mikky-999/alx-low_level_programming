ad82479f255137dbf1e3c711920d5a1927a43bf5
mikky-999 <michaeldaudu11@gmail.com>
2022-09-15 07:39:07 -0700

    2 and 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply two argument and return the result
+ *
+ * Return: result of multiplication
+ */
+
+int mul(int a, int c)
+{
+	return((a * c));
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print numbers from 1 to 9
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a)
+	}
+	_putchar('\n');
+}
