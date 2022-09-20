e3b2aa7f2b3d3e614aebaaa75a60f0b04c7b153d
mikky-999 <michaeldaudu11@gmail.com>
2022-09-19 20:23:16 -0700

    task 11

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _puts2 - This function prints a string, followed by a new line, to stdout
+ * puts2 - This function prints a string, followed by a new line, to stdout
  * @str: string to be printed
  *
  * Return: nothing
