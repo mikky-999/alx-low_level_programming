114955eec02a0e84d49e192d326a1c2e270d2097
mikky-999 <michaeldaudu11@gmail.com>
2022-09-29 21:52:43 +0800

    task 1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** _strlen_recursion - The function prints out a string in reverse
+ * *s: string variable
+ * Return: *s
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return 0;
+	else
+	{
+		_strlen_recursion(s + 1);
+		i++;
+	}
+	return 0;
+}
