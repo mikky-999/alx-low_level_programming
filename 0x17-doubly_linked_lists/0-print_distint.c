ac37fa74f220435308a358bf1388256896844353
mikky-999 <michaeldaudu11@gmail.com>
2022-12-10 02:20:07 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_distint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ *
+ * @count - Number of nodes 
+ * Return - Count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+	count = 0;
+
+	while(h != NULL)
+	{
+		printf("%d\n, h -> next")
+		count++
+	}
+	return(count)
+}
